92a7ab25d30e34a65fcfb12c5b4fd73fdf8680a3
pardamike <pardamike@gmail.com>
2016-03-19 12:31:24 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>		
+		<meta charset="utf-8">
+		<meta name="description" content="Tampa Pizza Company" />
+		<meta name="keywords" content="zillow api">
+    	<meta name="author" content="Mike Parda" />
+   		<meta name="robots" content="index" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1" >
+		<title>Mike Parda - Zillow API</title>
+		<link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/styles.css" rel="stylesheet">
+		<link rel="icon" href="img/zillow.png" />
+		
+		<!--[if lt IE 9]>
+			<script src="html5shiv.js"></script>
+			<script src="respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		
+		<?php include_once 'header.php' ;?>
+		
+		<h1>hello</h1>
+		
+				
+		<?php include_once 'footer.php'; ?>
+		
+		<script src="jquery/dist/jquery.min.js" type="text/javascript"></script>
+		<script src="bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+		<script src="js/jquery.globalControls.js" type="text/javascript"></script>
+	</body>
+	
+</html>
